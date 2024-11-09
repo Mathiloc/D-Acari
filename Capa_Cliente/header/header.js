d5dd777b93219c3417bc0515e4d022bcd33b2c96
Mathiloc <medranomathias45@gmail.com>
2024-11-09 12:32:16 -0500

    Se agrego el header,js

---
+++ Capa_Cliente/header/header.js
@@ -0,0 +1,55 @@
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+header.innerHTML = `<nav>
+    <link rel="stylesheet" href="../Estilos/header.css">
+
+        <div class="logo">
+            <img src="../Img/imagenes/logoredondo.png" alt="Logo Image">
+        </div>
+        <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Sobre Nosotros</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Reservas</a></li>
+            <li><a href="#">Club</a></li>
+            <li><a href="#">Registrate</a></li>
+            <li><button class="login-button" href="#">Login</button></li>
+            <li><button class="join-button" href="#">car</button></li>
+        </ul>
+    </nav>
+
+<script src="ban.js"></script>
+`;
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+const navbar = document.querySelector("nav");
+
+// Función que detecta el scroll y aplica la clase
+window.addEventListener("scroll", () => {
+    if (window.scrollY === 0) {
+        navbar.classList.add("transparent");
+    } else {
+        navbar.classList.remove("transparent");
+    }
+});
+
+// Inicializa el estado del navbar como transparente
+navbar.classList.add("transparent");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
